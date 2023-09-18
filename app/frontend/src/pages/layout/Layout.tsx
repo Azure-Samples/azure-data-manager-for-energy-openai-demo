402f3aac990bede16f728975e431695d0fc94ec4
Jon Olav Abeland <66827383+joasoze@users.noreply.github.com>
2023-09-18 13:40:38 +0200

    Update Layout.tsx

--- app/frontend/src/pages/layout/Layout.tsx
+++ app/frontend/src/pages/layout/Layout.tsx
@@ -20,11 +20,6 @@ const Layout = () => {
                                 </NavLink>
                             </li>
                             <li className={styles.headerNavLeftMargin}>
-                                <NavLink to="/qa" className={({ isActive }) => (isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink)}>
-                                    Ask a question
-                                </NavLink>
-                            </li>
-                            <li className={styles.headerNavLeftMargin}>
                                 <a href="https://github.com/Azure-Samples/azure-data-manager-for-energy-openai-demo" target={"_blank"} title="Github repository link">
                                     <img
                                         src={github}
